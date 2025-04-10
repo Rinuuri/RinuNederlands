4ffbb007cf79ea4cd1a1f61f71f457b82dac3606
rinuuri <me@rinuuri.dev>
2025-04-11 02:01:08 +0300

    Make the app actually open and add it to the unlocked list

--- app/src/main/java/dev/rinuuri/rinunederlands/AppLaunchMonitor.kt
+++ app/src/main/java/dev/rinuuri/rinunederlands/AppLaunchMonitor.kt
@@ -5,9 +5,13 @@ import android.accessibilityservice.AccessibilityServiceInfo
 import android.content.Intent
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
-import android.widget.Toast
+import java.time.Clock
 
 class AppLaunchMonitor : AccessibilityService() {
+
+    companion object {
+        val unlocked = HashMap<String, Long>()
+    }
 
     override fun onServiceConnected() {
         Log.i("service", "Service connected")
@@ -23,12 +27,20 @@ class AppLaunchMonitor : AccessibilityService() {
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
         if (event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
             val packageName = event.packageName.toString()
-            Log.i("service", packageName)
-            Toast.makeText(this, packageName, Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, packageName, Toast.LENGTH_SHORT).show()
             if (packageName == "com.google.android.youtube") {
+                val i =unlocked[packageName].toString();
+                val t = Clock.systemUTC().millis()/1000;
+                Log.i("service", "111 $i $t")
+                if (unlocked.containsKey(packageName)
+                    && unlocked[packageName]!! > Clock.systemUTC().millis()/1000) {
+                    return
+                }
+
                 // Launch your own activity instead
                 val intent = Intent(this, Popup::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                intent.putExtra("package_id",packageName)
                 startActivity(intent)
             }
         }
--- app/src/main/java/dev/rinuuri/rinunederlands/Popup.kt
+++ app/src/main/java/dev/rinuuri/rinunederlands/Popup.kt
@@ -1,30 +1,47 @@
 package dev.rinuuri.rinunederlands
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.time.Clock
 
 
 class Popup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popup)
         // Show your window or layout here
         // ...
         // Get the launch intent of the target app
-        val launchIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableExtra("launch_intent", Intent::class.java)
-        } else {
-            intent.getParcelableExtra("launch_intent");
-        }
 
         findViewById<Button>(R.id.yes).setOnClickListener {
-            findViewById<TextView>(R.id.textView).text = "Ik hou van Belgee"
+            findViewById<TextView>(R.id.textView).text = "Ik hou van Belgiee"
+        }
+
+        findViewById<Button>(R.id.open).setOnClickListener {
+            val id = intent.extras?.getString("package_id")
+            if (id != null) {
+                AppLaunchMonitor.unlocked[id] = Clock.systemUTC().millis()/1000+15
+                Log.i("service", AppLaunchMonitor.unlocked[id].toString())
+                Log.i("service", id)
+                val intent = Intent(Intent.ACTION_MAIN)
+                intent.setPackage(id)
+                intent.addCategory(Intent.CATEGORY_LAUNCHER)
+                startActivity(intent)
+                /*val launchIntent = packageManager.getLaunchIntentForPackage(id);
+                if (launchIntent != null) {
+                    launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(launchIntent)
+                } else {
+                    Log.e("popup", "intent = null")
+                }*/
+            } else Log.e("popup", "id = null")
         }
 
         // Launch the target app
-        startActivity(launchIntent)
+        //startActivity(launchIntent)
     }
 }
